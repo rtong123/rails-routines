09588ad608a2d82ee42f47946372bb7526e6d8d5
Rebecca Tong <rtong@buffalo.edu>
2019-06-03 13:23:48 -0400

    was able to allow user to logout and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
    protect_from_forgery with: :exception
 
+   def logged_in?
+      current_user.id != nil
+    end
+
+    def current_user
+   @user = (User.find_by(id: session[:user_id]) || User.new)
+ end
 
 
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,22 @@
+require 'pry'
 class SessionsController < ApplicationController
 
   def new
   end
 
   def create
-     @user = User.find_by(name: params[:user][:name])
+     @user = User.find_by(username: params[:username])
    if @user && @user.authenticate(params[:password])
      session[:user_id] = @user.id
-     redirect_to user_path(@user)
+     redirect_to '/users'
    else
      redirect_to '/users/new'
    end
  end
+
+  def destroy
+    session.clear
+    redirect_to '/'
+  end
 
 end
