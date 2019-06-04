e98fcc740c262fa67061b177e19664f8ab83a607
Rebecca Tong <rtong@buffalo.edu>
2019-06-04 17:05:09 -0400

    started edit page for routines

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -3,4 +3,22 @@ class RoutinesController < ApplicationController
     @routines = Routine.all
   end
 
+  def new
+    @routine = Routine.new
+  end
+
+  def create
+    @routine = Routine.create(routine_params)
+  end
+
+  def edit
+    @routine = Routine.find_by(params[:id])
+  end
+
+private
+
+  def routine_params
+    params.require(:routine).permit(:category, :title, :step)
+  end
+
 end
