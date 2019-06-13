119e8ef2769cb7f413877bfe93728d138939eaf6
Rebecca Tong <rtong@buffalo.edu>
2019-06-12 22:54:38 -0400

    allowing users to use facebook omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,37 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-     user = User.find_by(username: params[:username])
-   if user && user.authenticate(params[:password])
-     session[:user_id] = user.id
-     redirect_to user_path(user.id)
-   else
-    flash[:error] = "Sorry, we could not find the combination, try again or signup."
-     redirect_to '/users/new'
-   end
- end
+ #  def create
+ #     user = User.find_by(username: params[:username])
+ #   if user && user.authenticate(params[:password])
+ #     session[:user_id] = user.id
+ #     redirect_to user_path(user.id)
+ #   else
+ #    flash[:error] = "Sorry, we could not find the combination, try again or signup."
+ #     redirect_to '/users/new'
+ #   end
+ # end
 
   def destroy
     session.clear
     redirect_to '/'
+  end
+
+  def create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+    end
+
+    session[:user_id] = @user.id
+    render "users/show"
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_202309) do
+ActiveRecord::Schema.define(version: 2019_06_12_215348) do
 
   create_table "products", force: :cascade do |t|
     t.string "brand"
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 2019_05_23_202309) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "email"
+    t.string "image"
   end
 
 end
